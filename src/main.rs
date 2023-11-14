388de03b68f8cb446320b16011bc0e91592f8072
Roman Avdeev <lll_Death_lll@outlook.com>
2023-11-14 20:18:57 +0400

    Change algorithm for checking if point is inside the triangle

--- src/main.rs
+++ src/main.rs
@@ -31,12 +31,12 @@ fn main() {
         };
 
         // Caclulating min and max x and y coordinates
-        let x_min = [x[0], y[0], z[0]].iter().min().unwrap().to_owned();
-        let x_max = [x[0], y[0], z[0]].iter().max().unwrap().to_owned();
-        let y_min = [x[1], y[1], z[1]].iter().min().unwrap().to_owned();
-        let y_max = [x[1], y[1], z[1]].iter().max().unwrap().to_owned();
+        let x_min = [x[0], y[0], z[0]].iter().fold(f32::INFINITY, |a, &b| a.min(b)) as i32;
+        let x_max = [x[0], y[0], z[0]].iter().fold(f32::NEG_INFINITY, |a, &b| a.max(b)) as i32;
+        let y_min = [x[1], y[1], z[1]].iter().fold(f32::INFINITY, |a, &b| a.min(b)) as i32;
+        let y_max = [x[1], y[1], z[1]].iter().fold(f32::NEG_INFINITY, |a, &b| a.max(b)) as i32;
 
-        let mut count = 0; // Setting coordinates count to zero by default
+        let mut coordinate_count = 0; // Setting coordinates count to zero by default
 
         /*
         Looping through each coordinate
@@ -44,55 +44,64 @@ fn main() {
         */
         for i in x_min..=x_max {
             for j in y_min..=y_max {
-                // Calculating triangle area
-                let triangle_area =
-                    ((y[0] - x[0]) * (z[1] - x[1]) - (z[0] - x[0]) * (y[1] - x[1])).abs() / 2;
-                /*
-                Calculating barycentric coordinates
-                (How close point is to the vertexes)
-                */
-                let a = ((y[0] - i) * (z[1] - j) - (z[0] - i) * (y[1] - j)).abs() / 2;
-                let b = ((z[0] - i) * (x[1] - j) - (x[0] - i) * (z[1] - j)).abs() / 2;
-                let c = ((x[0] - i) * (y[1] - j) - (y[0] - i) * (x[1] - j)).abs() / 2;
-
-                /*
-                If sum of all coordinates is
-                equals to area adding one to
-                coordinates count
-                */
-                if triangle_area >= a + b + c {
-                    count += 1;
+                let point = [i as f32, j as f32];
+                if is_point_in_triangle(point, x, y, z) {
+                    coordinate_count += 1;
                 }
             }
         }
 
-        println!("Total points ammount: {}\n", count);
+        println!("Total points ammount: {}\n", coordinate_count);
     }
 }
 
-// Returns array of two i32
-fn get_coordinate() -> Result<[i32; 2], String> {
-    let x1 = get_i32()?;
-    let x2 = get_i32()?;
+// Checks if point is in triangle.
+fn is_point_in_triangle(point: [f32; 2], x: [f32; 2], y: [f32; 2], z: [f32; 2]) -> bool {
+    /*
+    Check the sign of the areas of
+    smaller triangles formed by the
+    point and pairs of vertices
+     */
+    let a = sign(point, x, y) < 0.0;
+    let b = sign(point, y, z) < 0.0;
+    let c = sign(point, z, x) < 0.0;
+
+    // Returns true, if the signs are all the same (inside the triangle)
+    return a == b && b == c;
+}
+
+// Calculates the sign of area formed by point.
+fn sign(x: [f32; 2], y: [f32; 2], z: [f32; 2]) -> f32 {
+    return (x[0] - z[0]) * (y[1] - z[1]) - (y[0] - z[0]) * (x[1] - z[1]);
+}
+
+// Returns array of two doubles
+fn get_coordinate() -> Result<[f32; 2], String> {
+    let x1 = get_f32()?;
+    let x2 = get_f32()?;
 
     Ok([x1, x2])
 }
 
-// Gets user input and converts it to i32
-fn get_i32() -> Result<i32, String> {
+// Gets user input and converts it to f32
+fn get_f32() -> Result<f32, String> {
     let mut x = String::new();
 
     io::stdin().read_line(&mut x).expect("Failed to read line");
 
-    let x: Result<i32, String> = match x.trim().parse() {
+    let x: Result<f32, String> = match x.trim().parse() {
         Ok(num) => Ok(num),
         // Panics on errors with additional comments
-        Err(err) => match err.kind() {
-            std::num::IntErrorKind::InvalidDigit => Err(format!("Value must be a number! {}", err)),
-            std::num::IntErrorKind::PosOverflow => Err(format!("Value is too big! {}", err)),
-            std::num::IntErrorKind::NegOverflow => Err(format!("Value is too small! {}", err)),
-            std::num::IntErrorKind::Empty => Ok(0),
-            _ => panic!("Error! {}", err),
+        Err(err) => {
+            if err.to_string().contains("invalid") {
+                Err("Invalid format: Not a valid floating-point number".to_string())
+            } else if err.to_string().contains("overflow") {
+                Err("Overflow: Number is too large".to_string())
+            } else if err.to_string().contains("underflow") {
+                Err("Underflow: Number is too small".to_string())
+            } else {
+                Err("Unknown error occurred".to_string())
+            }
         },
     };
 
